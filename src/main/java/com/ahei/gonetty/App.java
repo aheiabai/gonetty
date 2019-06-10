b6b59d42243fae9cb190760073db34492a296a81
ahei <aheiabaia@gmail.com>
2019-06-10 11:44:36 +0800

    Come with ehcache for user cache

--- src/main/java/com/ahei/gonetty/App.java
+++ src/main/java/com/ahei/gonetty/App.java
@@ -1,13 +1,36 @@
 package com.ahei.gonetty;
 
+import java.time.Duration;
+
+import org.ehcache.CacheManager;
+import org.ehcache.config.Builder;
+import org.ehcache.config.CacheConfiguration;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.CacheManagerBuilder;
+import org.ehcache.config.builders.ExpiryPolicyBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.core.spi.service.StatisticsService;
+import org.ehcache.core.statistics.CacheStatistics;
+import org.ehcache.impl.internal.statistics.DefaultStatisticsService;
+import org.ehcache.spi.service.Service;
+import org.ehcache.spi.service.ServiceProvider;
+
 /**
  * Hello world!
  *
  */
 public class App {
+	public static  final StatisticsService statisService= new DefaultStatisticsService();
+	public static final CacheConfiguration<String, String> cacheConfig = CacheConfigurationBuilder.newCacheConfigurationBuilder(
+			String.class, String.class, ResourcePoolsBuilder.heap(1007))
+			.withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofHours(2)))
+			.build();
+	public static final CacheManager CACHE_MANAGER = CacheManagerBuilder.newCacheManagerBuilder()
+			.using(statisService)
+			.build(true);
+
 	public static void main(String[] args) {
 		int port = 8080;
-		
 		if (args.length > 0) {
 			port = Integer.parseInt(args[0]);
 		}
@@ -18,4 +41,5 @@ public class App {
 		 */
 		new Server(port).run();
 	}
+	
 }
--- src/main/java/com/ahei/gonetty/MessageHandler.java
+++ src/main/java/com/ahei/gonetty/MessageHandler.java
@@ -3,6 +3,8 @@ package com.ahei.gonetty;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.ehcache.Cache;
+import org.ehcache.core.statistics.CacheStatistics;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,36 +18,54 @@ import io.netty.util.concurrent.GlobalEventExecutor;
 
 public class MessageHandler extends SimpleChannelInboundHandler<String> {
 	private static final Logger logger = LoggerFactory.getLogger(MessageHandler.class);
-	private static final ChannelGroup channels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+//	private static final ChannelGroup channels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
 	private String channelId;
 	private static final Map<String, Channel> CHANNEL_MAP = new HashMap<>();
+
+	private static final Cache<String, String> cache = App.CACHE_MANAGER.createCache("idCache", App.cacheConfig);
+	CacheStatistics stats = App.statisService.getCacheStatistics("idCache");
 
 	@Override
 	public void channelActive(ChannelHandlerContext ctx) throws Exception {
 //		channels.add(ctx.channel());
 //		channelId = ((NioSocketChannel)ctx.channel()).remoteAddress().hashCode();
-//		String host = ((NioSocketChannel)ctx.channel()).remoteAddress().getHostString();
-//		int port = ((NioSocketChannel)ctx.channel()).remoteAddress().getPort();
+		String host = ((NioSocketChannel) ctx.channel()).remoteAddress().getHostString();
+		int port = ((NioSocketChannel) ctx.channel()).remoteAddress().getPort();
 //		logger.debug("host: {}, port: {}", host, port);
 
-		channelId = NameGenerator.generator();
-		CHANNEL_MAP.put(channelId, ctx.channel());
-		logger.debug("New channelId {}, remaining channels {}", channelId, CHANNEL_MAP.size());
-		ctx.writeAndFlush("Welcome " + channelId + "!\r\n");
-
-		for (Channel c : CHANNEL_MAP.values()) {
-			c.writeAndFlush("Online " + CHANNEL_MAP.size() + "\r\n");
-		}
+//		logger.debug("New channelId {}, remaining channels {}", channelId, CHANNEL_MAP.size());
+		logger.info("new channel --> address: {}:{}", host, port);
 	}
 
 	@Override
 	protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
 		if ("quit".equals(msg)) {
+			cache.remove(channelId);
 			ctx.close();
 //			channels.remove(ctx.channel());
 //			System.out.println(channelId + " is leaving, remaining channels " + channels.size());
 //			logger.debug(channelId + " is leaving, remaining channels " + channels.size());
-		} else {
+		} else if (msg.startsWith("--UID ")) {// keep user for 2 hour
+			channelId = msg.substring(6, msg.length());
+			if (cache.containsKey(channelId)) {
+				cache.put(channelId, "");
+				CHANNEL_MAP.put(channelId, ctx.channel());
+				logger.debug("{} reconnect, cache size {}", channelId, stats.getTierStatistics().get("OnHeap").getMappings());
+			} else {
+				channelId = NameGenerator.generator();
+				CHANNEL_MAP.put(channelId, ctx.channel());
+				cache.put(channelId, "");
+				ctx.writeAndFlush("Welcome " + channelId + "!\r\n");
+				logger.debug("New User {}, remaining channels {}, cache size {}", channelId, CHANNEL_MAP.size(),
+						stats.getTierStatistics().get("OnHeap").getMappings());
+			}
+			
+			// broadcast online number
+			for (Channel c : CHANNEL_MAP.values()) {
+				c.writeAndFlush("Online " + CHANNEL_MAP.size() + "\r\n");
+			}
+
+		} else {// broadcast msg
 			for (Channel c : CHANNEL_MAP.values()) {
 				c.writeAndFlush(channelId + " say " + msg + "\r\n");
 			}
